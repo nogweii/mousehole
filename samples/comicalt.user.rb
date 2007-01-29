0fca4bb62ba78b42fcf013e2f9bf15148017255e
Leslie Wu <lwu.two@gmail.com>
2007-01-29 08:55:50 +0000

    Upgrade comicalt.user.rb sample from MH1 to MH2

--- samples/comicalt.user.rb
+++ samples/comicalt.user.rb
@@ -1,34 +1,35 @@
-MouseHole.script do
-    # declaration
-    name "Comics Alt Text" 
-    namespace "http://adamv.com/greases/" 
+# Simple MouseHole 2.0 script, based on the Greasemonkey script
+# by Adam Vandenberg.  His is GPL, so this is GPL.
+# <http://adamv.com/dev/grease/scripts/comicalt.user.js>
+class ComicAlt < MouseHole::App
+    title "Comics Alt Text"
+    namespace "http://adamv.com/greases/"
     description 'Shows the "hover text" for some comics on the page'
-    include_match %r!^http://.*achewood\.com/!
-    include_match %r!^http://.*qwantz\.com/!
-    version "0.2" 
+    version "0.3"
+    + url("http://achewood.com/*")
+    + url("http://*.achewood.com/*")
+    + url("http://qwantz.com/*")
+    + url("http://*.qwantz.com/*")
 
-    # instance variables
-    @comics = {
-        'achewood' => '//img[starts-with(@src, "/comic.php?date=")]',
-        'qwantz'   => '//img[starts-with(@src, "/comics/")]'
-    }
+    COMICS = {
+        "achewood" => 'img[@src^="/comic.php?date="]',
+        "qwantz" => 'img[@src^="http://www.qwantz.com/comics/"]'
+    }     
 
     # the pages flow through here
-    rewrite do |req, res|
-        whichSite = case req.request_uri.host
-            when /achewood/: "achewood" 
-            when /qwantz/: "qwantz" 
-        end
+    def rewrite(page)
+        whichSite, xpath =
+            COMICS.detect do |key,|
+                page.location.host.include? key
+            end
         return unless whichSite
 
-        comic = document.elements[@comics[whichSite]]
+        comic = document.at(xpath)
         return unless comic
 
-        if comic.attributes['title']
-            div = Element.new 'div'
-            div.attributes['className'] = 'msg'
-            div.text = "(#{ comic.attributes['title'] })" 
-            comic.parent.insert_after comic, div
+        if comic['title']
+          div = Hpricot.make("<div class='msg'>(#{ comic['title'] })</div>")
+          comic.parent.insert_after div, comic
         end
     end
 end
