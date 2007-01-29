01c9c52781b689deca2477beeb54b33dd57423bc
Leslie Wu <lwu.two@gmail.com>
2007-01-29 08:54:22 +0000

    Update mousehole sample script from MH1 to MH2, based on http://whytheluckystiff.net/mouseHole/

--- samples/proxylike.user.rb
+++ samples/proxylike.user.rb
@@ -1,27 +1,38 @@
-MouseHole.script do
-    name 'ProxyLike'
-    namespace 'http://whytheluckystiff.net/mouseHole/'
-    description %{
-        Run pages through the proxy by passing them in on the URL.
-        For example, to view Boing Boing through the proxy, use:
-        http://localhost:37004/http://boingboing.net/ 
-    }
-    version '0.2'
+require 'open-uri'
 
-    mount "http:" do |path_info|
-        mH = request.request_uri.dup
-        mH.path = '/'
-        uri = URI.parse("http://#{ path_info }")
-        uri.path = '/' if uri.path.empty?
-        open(uri, :proxy => mH) do |f|
-            base_uri = uri.dup
-            base_uri.path = '/'
-            base_href f.read, base_uri
-        end
+class Array
+  def to_h
+    self.inject({}) do |hash, value|
+      hash[value.first] = value.last ; hash
     end
+  end
+end
 
-    def base_href( html, uri )
-        html.gsub( /(href\s*=\s*["']?)(#{ uri }|\/+)/, "\\1http://#{ MOUSEHOST }:#{ MOUSEPORT }/#{ uri }" ).
-             gsub( /<head>/, %(<head><base href="#{ uri }" />) )
+class ProxyLike < MouseHole::App
+  title 'ProxyLike'
+  namespace 'http://whytheluckystiff.net/mouseHole/'
+  description %{
+    Run pages through the proxy by passing them in on the URL.
+    For example, to view Boing Boing through the proxy, use:
+    http://localhost:37004/http://boingboing.net/ 
+  }
+  version '2.1'
+  
+  mount "http:" do |page|
+    mH = "http://#{ page.headers['host'] }/"
+    uri = URI(page.location.to_s[1..-1])
+    options = {:proxy => mH}.merge(page.headers.to_h)
+    options.delete "host"
+    page.document =
+      uri.open(options) do |f|
+      base_uri = uri.dup
+      base_uri.path = '/'
+      base_href f.read, base_uri, mH
     end
+  end
+
+  def self.base_href( html, uri, mh )
+    html.gsub( /(href\s*=\s*["']?)(#{ uri }|\/+)/, "\\1#{ mh }#{ uri }" ).
+      sub( /<html/i, %(<base href="#{ uri }" /><html) )
+  end
 end
