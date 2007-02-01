3720e3e21ed96a6739017d039278cf11255eea4a
Leslie Wu <lwu.two@gmail.com>
2007-02-01 00:15:09 +0000

    Add descriptive info in the coral.rb comments

--- samples/coral.rb
+++ samples/coral.rb
@@ -1,3 +1,17 @@
+# This is a script written for why's MouseHole (2.0+), the personal web proxy,
+# as part of an effort to support "environmentally friendly!" web browsing.
+#
+# Whenever you visit a site such as slashdot or digg (through your own personal
+# MouseHole proxy), this script will rewrite http links to use the Coral Content
+# Distribution Network. Thus, it encourages web surfers to be responsible and
+# avoid oversaturating the infrastructure of the long tail of the Web. 
+#
+# ~L
+#
+# For more information on installing and configuring the MouseHole, see
+#   http://code.whytheluckystiff.net/mouseHole
+#
+
 class Coral < MouseHole::App
   title 'Coral'
   namespace 'http://www.stanford.edu'
