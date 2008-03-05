f3c55cd3288f1931c4a9774f85e0681fc72c1038
C Z <netshade@gmail.com>
2008-03-05 14:28:40 +0000

    * fixed bad mime extraction for page converters

--- lib/mouseHole/page.rb
+++ lib/mouseHole/page.rb
@@ -23,10 +23,10 @@ module MouseHole
       @status = status
       @input = Camping.qsp(uri.query)
       @headers = PageHeaders[*headers]
-      ctype = @headers['Content-Type']
+      ctype = @headers['Content-Type'].split(";")
       ctype = ctype.first if ctype.respond_to? :first
       if ctype
-        @converter = Converters.detect_by_mime_type ctype.split(';',2)[0]
+        @converter = Converters.detect_by_mime_type ctype
       end
     end
 
