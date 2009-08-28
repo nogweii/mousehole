a521ad704cb43215c3015164da0cb238bad583b9
Colin Shea <colin@evaryont.me>
2009-08-28 11:45:48 -0400

    Not all clients send Content-Type, therefore check before creating a converter Signed-off-by: Colin Shea <colin@evaryont.me>

--- lib/mouseHole/page.rb
+++ lib/mouseHole/page.rb
@@ -23,10 +23,12 @@ module MouseHole
       @status = status
       @input = Camping.qsp(uri.query)
       @headers = PageHeaders[*headers]
-      ctype = @headers['Content-Type'].split(";")
-      ctype = ctype.first if ctype.respond_to? :first
-      if ctype
-        @converter = Converters.detect_by_mime_type ctype
+      if @headers['Content-Type']
+        ctype = @headers['Content-Type'].split(";")
+        ctype = ctype.first if ctype.respond_to? :first
+        if ctype
+          @converter = Converters.detect_by_mime_type ctype
+        end
       end
     end
 
